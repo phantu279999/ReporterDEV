b0db7d15692b7691eca3665fae5fdd25bc5eac02
phantu279999 <phantu279999@gmail.com>
2024-12-15 19:51:27 +0700

    implement page news in zones

--- core/news/urls.py
+++ core/news/urls.py
@@ -6,5 +6,6 @@ app_name = 'news'
 
 urlpatterns = [
 	path('', views.list_news_view, name='list_news'),
-	path('<slug:slug>/', views.detail_news_view, name='detail_news')
+	path('detail/<slug:slug>/', views.detail_news_view, name='detail_news'),
+	path('zone/<slug:url>/', views.news_in_zone, name='news_in_zone'),
 ]
--- core/news/views.py
+++ core/news/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 
-from .models import News
+from .models import News, Zone, NewsInZone
+
+
+def news_in_zone(request, url):
+	zone = Zone.objects.get(url=url)
+	news = [item.news for item in NewsInZone.objects.filter(zone__url=url).select_related('news')]
+	return render(request, 'news/news_in_zone.html', {'zone': zone, 'news': news})
 
 
 def list_news_view(request):
+	zones = Zone.objects.all()
 	news = News.objects.all()
 
-	return render(request, 'news/list_news.html', {'news': news})
+	return render(
+		request,
+		'news/list_news.html',
+		{'zones': zones, 'news': news}
+	)
 
 
 def detail_news_view(request, slug):
