282c4558f053bdcfffb09c3c85995a6be7dbc673
phantu279999 <phantu279999@gmail.com>
2025-01-14 15:38:42 +0700

    using CBV in blog

--- core/blogs/urls.py
+++ core/blogs/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 
 
 urlpatterns = [
-	path('', views.list_blog_view, name='list_blog'),
-	path('detail/<slug:slug>/', views.detail_blog_view, name='detail_blog'),
+	path('', views.BlogListView.as_view(), name='list_blog'),
+	path('detail/<slug:slug>/', views.DetailBlogView.as_view(), name='detail_blog'),
 ]
--- core/blogs/views.py
+++ core/blogs/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from core.blogs.models import Blog, Category
 
 
-def list_blog_view(request):
-	blogs = Blog.objects.all()
-	categories = Category.objects.all()
-	return render(request, 'blogs/list_blogs.html', {'blogs': blogs, 'categories': categories})
+class BlogListView(ListView):
+	model = Blog
+	template_name = 'blogs/list_blogs.html'
+	context_object_name = 'blogs'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categories'] = Category.objects.all()
+		return context
 
 
-def detail_blog_view(request, slug):
-	blog = Blog.objects.get(slug=slug)
-	return render(request, 'blogs/detail_blog.html', {'blog': blog})
+class DetailBlogView(DetailView):
+	model = Blog
+	template_name = 'blogs/detail_blog.html'
+	context_object_name = 'blog'
+
+
+# def list_blog_view(request):
+# 	blogs = Blog.objects.all()
+# 	categories = Category.objects.all()
+# 	return render(request, 'blogs/list_blogs.html', {'blogs': blogs, 'categories': categories})
+
+
+# def detail_blog_view(request, slug):
+# 	blog = Blog.objects.get(slug=slug)
+# 	return render(request, 'blogs/detail_blog.html', {'blog': blog})
