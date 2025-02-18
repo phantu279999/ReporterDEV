99a131a48dd9ff2fb62028275c7bfc8013c6b3f7
phantu279999 <phantu279999@gmail.com>
2025-02-18 19:08:14 +0700

    add block tags on home

--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-from core.news.models import News
+from core.news.models import News, Tag
 from core.blogs.models import Blog
 
 
 def index(request):
 	news = News.objects.all()[:6]
 	blogs = Blog.objects.all()[:6]
-	return render(request, 'index.html', {'news': news, 'blogs': blogs})
+	tags = Tag.objects.all()[:10]
+	context = {
+		'news': news,
+		'blogs': blogs,
+		'tags': tags,
+	}
+	return render(request, 'index.html', context=context)
